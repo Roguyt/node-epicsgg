4d87be4d3efe75dd75b5a911b866c0c19d783353
Clément Weingaertner <clem.weingaertner@gmail.com>
2019-07-08 10:40:46 +0200

    feat(UserActions): implement getShowcasedCards

--- src/client/actions/Leaderboard.ts
+++ src/client/actions/Leaderboard.ts
@@ -7,35 +7,36 @@ export default class Leaderboard {
         this.baseClient = baseClient;
     }
 
-    public getLeaderboards(page: number = 1): Promise<UserRanking[]> {
-        return this.baseClient.get('leaderboards/categories/1?categoryId=1&gameId=1&page=' + page + '')
+    public getLeaderboards(page: number = 1, categoryId: number = 1, gameId: number = 1): Promise<UserRanking[]> {
+        return this.baseClient
+            .get('leaderboards/categories/1?categoryId=' + categoryId + '&gameId=' + gameId + '&page=' + page + '')
             .then(
-            (result): Promise<UserRanking[]> => {
-                return new Promise((resolve): void => {
-                    const data: UserRanking[] = [];
+                (result): Promise<UserRanking[]> => {
+                    return new Promise((resolve): void => {
+                        const data: UserRanking[] = [];
 
-                    for (let i = 0; i < result.length; i += 1) {
-                        let ranking = result[i];
+                        for (let i = 0; i < result.length; i += 1) {
+                            let ranking = result[i];
 
-                        data.push({
-                            id: ranking.user.id,
-                            username: ranking.user.username,
-                            avatar: ranking.user.avatar,
-                            group: ranking.user.group,
-                            country: ranking.user.country,
-                            joined: ranking.user.created,
-                            ranking: {
-                                rank: ranking.rank,
-                                score: ranking.score,
-                                cardCount: ranking.cardCount,
-                                entityCount: ranking.entityCount,
-                            },
-                        });
-                    }
+                            data.push({
+                                id: ranking.user.id,
+                                username: ranking.user.username,
+                                avatar: ranking.user.avatar,
+                                group: ranking.user.group,
+                                country: ranking.user.country,
+                                joined: ranking.user.created,
+                                ranking: {
+                                    rank: ranking.rank,
+                                    score: ranking.score,
+                                    cardCount: ranking.cardCount,
+                                    entityCount: ranking.entityCount,
+                                },
+                            });
+                        }
 
-                    resolve(data);
-                });
-            }
-        );
+                        resolve(data);
+                    });
+                }
+            );
     }
 }
