92be97616f78ff6b62b0a46d2fa4410c9122ae76
Clément Weingaertner <clem.weingaertner@gmail.com>
2019-07-09 08:01:18 +0200

    :sparkles: [Leaderboard] Added the country flag

--- src/client/actions/Leaderboard.ts
+++ src/client/actions/Leaderboard.ts
@@ -7,36 +7,45 @@ export default class Leaderboard {
         this.baseClient = baseClient;
     }
 
-    public getLeaderboards(page: number = 1, categoryId: number = 1, gameId: number = 1): Promise<UserRanking[]> {
-        return this.baseClient
-            .get('leaderboards/categories/1?categoryId=' + categoryId + '&gameId=' + gameId + '&page=' + page + '')
-            .then(
-                (result): Promise<UserRanking[]> => {
-                    return new Promise((resolve): void => {
-                        const data: UserRanking[] = [];
+    public getLeaderboards(
+        page: number = 1,
+        country: string = null,
+        categoryId: number = 1,
+        gameId: number = 1
+    ): Promise<UserRanking[]> {
+        let url = 'leaderboards/categories/1?categoryId=' + categoryId + '&gameId=' + gameId + '&page=' + page + '';
 
-                        for (let i = 0; i < result.length; i += 1) {
-                            let ranking = result[i];
+        if (country !== null) {
+            url += '&country=' + country;
+        }
 
-                            data.push({
-                                id: ranking.user.id,
-                                username: ranking.user.username,
-                                avatar: ranking.user.avatar,
-                                group: ranking.user.group,
-                                country: ranking.user.country,
-                                joined: ranking.user.created,
-                                ranking: {
-                                    rank: ranking.rank,
-                                    score: ranking.score,
-                                    cardCount: ranking.cardCount,
-                                    entityCount: ranking.entityCount,
-                                },
-                            });
-                        }
+        return this.baseClient.get(url).then(
+            (result): Promise<UserRanking[]> => {
+                return new Promise((resolve): void => {
+                    const data: UserRanking[] = [];
 
-                        resolve(data);
-                    });
-                }
-            );
+                    for (let i = 0; i < result.length; i += 1) {
+                        let ranking = result[i];
+
+                        data.push({
+                            id: ranking.user.id,
+                            username: ranking.user.username,
+                            avatar: ranking.user.avatar,
+                            group: ranking.user.group,
+                            country: ranking.user.country,
+                            joined: ranking.user.created,
+                            ranking: {
+                                rank: ranking.rank,
+                                score: ranking.score,
+                                cardCount: ranking.cardCount,
+                                entityCount: ranking.entityCount,
+                            },
+                        });
+                    }
+
+                    resolve(data);
+                });
+            }
+        );
     }
 }
