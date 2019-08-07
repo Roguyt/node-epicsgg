af6d3bfb256eaefa272c349a9b7ce3525480f0a9
Roguyt <clem.weingaertner@gmail.com>
2019-08-08 00:39:52 +0200

    :rotating_light: Fix ESLint warnings

--- src/client/utils/Date.ts
+++ src/client/utils/Date.ts
@@ -2,7 +2,7 @@
  * @hidden
  */
 export default class DateUtils {
-    public static convertToDate(date: string) {
+    public static convertToDate(date: string): Date {
         if (date === null) return null;
 
         return new Date(date);
